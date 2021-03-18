b3cc293a01584c8cfa17d325f92ffd4fef927d48
OnoprienkoArtem <o.artem.e@gmail.com>
2021-03-18 19:11:58 +0200

    change start script in packegejson

--- index.js
+++ index.js
@@ -2,17 +2,9 @@
 import task1 from './task1.js';
 import task2 from './task2.js';
 
-console.log(task1);
-
-const tasks = {
-    task1,
-    task2
-}
+const tasks = { task1, task2 };
 
 console.log(process.argv);
 console.log(process.argv[2]);
 
 tasks[process.argv[2]]();
-
-// task1();
-// task2();
