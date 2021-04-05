a8ed353edd8c0b603e2cff5276dc9b960a2d3756
OnoprienkoArtem <o.artem.e@gmail.com>
2021-04-05 23:06:57 +0300

    add types to res and req

--- hw-2/routes/routes.ts
+++ hw-2/routes/routes.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import { Request, Response } from 'express';
 
 import { getAutoSuggestUsers, getUserById, createUser, updateUserById, removeUserById } from '../utils/functions';
 import { schema } from '../validation/validationSchema';
@@ -6,15 +7,15 @@ import { validateSchema } from '../validation/validator';
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
+router.get('/', (req: Request, res: Response) => {    
     try {
-        res.json(getAutoSuggestUsers(req.query.login, Number(req.query.limit)));
+        res.send(getAutoSuggestUsers(req.query.login, Number(req.query.limit)));
     } catch (error) {
         res.status(404).send(error);
     }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', (req: Request, res) => {
     try {
         res.json(getUserById(req.params.id));
     } catch (error) {
@@ -22,7 +23,7 @@ router.get('/:id', (req, res) => {
     }
 });
 
-router.post('/', validateSchema(schema), (req, res) => {
+router.post('/', validateSchema(schema), (req: Request, res: Response) => {
     try {
         res.send(createUser(req.query));
     } catch (error) {
@@ -30,7 +31,7 @@ router.post('/', validateSchema(schema), (req, res) => {
     }
 });
 
-router.put('/:id', validateSchema(schema), (req, res) => {
+router.put('/:id', validateSchema(schema), (req: Request, res: Response) => {
     try {
         res.json(updateUserById(req.params.id, req.query));
     } catch (error) {
@@ -38,7 +39,7 @@ router.put('/:id', validateSchema(schema), (req, res) => {
     }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', (req: Request, res: Response) => {
     try {
         res.send(removeUserById(req.params.id));
     } catch (error) {
